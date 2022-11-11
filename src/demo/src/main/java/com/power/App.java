12359b78e2a2d94be36e523f7e54017e636292bd
Vijay Kethanaboyina <112053742+vijaykcsis@users.noreply.github.com>
2022-11-10 19:31:34 -0800

    added user input and input validation

--- src/demo/src/main/java/com/power/App.java
+++ src/demo/src/main/java/com/power/App.java
@@ -2,45 +2,71 @@ package com.power;
 
 import java.util.*;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        
+    {    
         Scanner scan = new Scanner(System.in);
-        Character command = '_';
-        /*
-        // This while loop runs until the user quits
-        // This snippet of code is adapted from the
-        // ChavviCalc assignment I submitted earlier  
-        // in the semester.
-        while (command != 'q') {
-            printMenu();
-            System.out.print("Enter a command: ");
-            command = menuGetCommand(scan);
-            executeCommand(scan, command, commandArray);
-        }
+        
+        char cont;
+
+        do {
+            cont = '_';
+            System.out.print("Please enter a value for x: ");
+            float x = Float.parseFloat(scan.nextLine());
+    
+            System.out.print("Please enter a value for n: ");
+            int n = Integer.parseInt(scan.nextLine());
+    
+            System.out.print("The value of x^n is... ");
+            System.out.println(power(x, n));
+            
+            while ((cont != 'y') && (cont != 'n')) { // Input validation
+                System.out.print("Would you like to continue using the program? ('y' = yes, 'n' = no): ");
+                // This snippet of code that reads the user input is 
+                // from the chavvicalc assignment that I submitted 
+                // earlier in the semester
+                cont = menuGetCommand(scan);
+                if ((cont != 'y') && (cont != 'n')) {
+                    System.out.println("ERROR: Invalid command");
+                }
+            }
+            if (cont == 'n') {
+                System.out.println("Quitting...");
+            }
+        } while (cont == 'y');
         scan.close();
-        */
-        System.out.println(power(2, 10));
+        System.out.println("Thank you for using the program.");
+    }
+
+    private static Character menuGetCommand(Scanner scan) {
+        Character command = '_';
+
+        String rawInput = scan.nextLine();
+
+        if (rawInput.length() > 0) {
+            rawInput = rawInput.toLowerCase();
+            command = rawInput.charAt(0);
+        }
+
+        return command;
     }
 
     public static float power(float x, int n) {
 
-        if (n == 0) {
+        if ((n == 0 && x == 0f) || (n < 0)) {
+            System.out.println("ERROR: Undefined, returning -1.");
+            return -1;
+        }
+        else if (n == 0) {
             return 1.0f;
         }
-        if (n == 1) {
+        else if (n == 1) {
             return x;
         }
 
         float result = x*power(x, n-1);
-        System.out.println("DEBUG PRINT: RESULT, X, N: " + String.valueOf(result) + ", " + String.valueOf(x) + ", " + String.valueOf(n));
+        //System.out.println("DEBUG PRINT: RESULT, X, N: " + String.valueOf(result) + ", " + String.valueOf(x) + ", " + String.valueOf(n));
         return result;
     }
 }
