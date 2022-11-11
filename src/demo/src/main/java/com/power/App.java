6da29ebc3bd3256f79c4251a1341e87f274ece41
Vijay Kethanaboyina <112053742+vijaykcsis@users.noreply.github.com>
2022-11-10 19:07:35 -0800

    create basic power function

--- src/demo/src/main/java/com/power/App.java
+++ src/demo/src/main/java/com/power/App.java
@@ -1,5 +1,7 @@
 package com.power;
 
+import java.util.*;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,36 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        Scanner scan = new Scanner(System.in);
+        Character command = '_';
+        /*
+        // This while loop runs until the user quits
+        // This snippet of code is adapted from the
+        // ChavviCalc assignment I submitted earlier  
+        // in the semester.
+        while (command != 'q') {
+            printMenu();
+            System.out.print("Enter a command: ");
+            command = menuGetCommand(scan);
+            executeCommand(scan, command, commandArray);
+        }
+        scan.close();
+        */
+        System.out.println(power(2, 10));
+    }
+
+    public static float power(float x, int n) {
+
+        if (n == 0) {
+            return 1.0f;
+        }
+        if (n == 1) {
+            return x;
+        }
+
+        float result = x*power(x, n-1);
+        System.out.println("DEBUG PRINT: RESULT, X, N: " + String.valueOf(result) + ", " + String.valueOf(x) + ", " + String.valueOf(n));
+        return result;
     }
 }
